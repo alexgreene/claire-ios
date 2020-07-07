bcf138dc9dfe2ad6b247777b29a661676fe27870
Alex Greene <alexgrn7@gmail.com>
2020-07-07 13:02:44 -0700

    Initial Commit

---
+++ Claire/ClaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaireApp.swift
+//  Claire
+//
+//  Created by Alex Greene on 7/7/20.
+//
+
+import SwiftUI
+
+@main
+struct ClaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
